4f33e7ebf2ebd2a025bd1bbdbcaa18d635edf1e1
Akshaj Kumar Veldanda <44175190+akshajkumarv@users.noreply.github.com>
2021-12-04 20:07:53 -0500

    Create eval.py

---
+++ lab3/eval.py
@@ -0,0 +1,27 @@
+import keras
+import sys
+import h5py
+import numpy as np
+
+clean_data_filename = str(sys.argv[1])
+model_filename = str(sys.argv[2])
+
+def data_loader(filepath):
+    data = h5py.File(filepath, 'r')
+    x_data = np.array(data['data'])
+    y_data = np.array(data['label'])
+    x_data = x_data.transpose((0,2,3,1))
+
+    return x_data, y_data
+
+def main():
+    x_test, y_test = data_loader(clean_data_filename)
+
+    bd_model = keras.models.load_model(model_filename)
+
+    clean_label_p = np.argmax(bd_model.predict(x_test), axis=1)
+    class_accu = np.mean(np.equal(clean_label_p, y_test))*100
+    print('Classification accuracy:', class_accu)
+
+if __name__ == '__main__':
+    main()

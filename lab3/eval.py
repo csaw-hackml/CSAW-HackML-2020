c39e4737070116cc9478c49a2ddb460fc87d2411
Akshaj Kumar Veldanda <44175190+akshajkumarv@users.noreply.github.com>
2021-12-04 20:38:28 -0500

    Update eval.py

--- lab3/eval.py
+++ lab3/eval.py
@@ -4,7 +4,8 @@ import h5py
 import numpy as np
 
 clean_data_filename = str(sys.argv[1])
-model_filename = str(sys.argv[2])
+poisoned_data_filename = str(sys.argv[2])
+model_filename = str(sys.argv[3])
 
 def data_loader(filepath):
     data = h5py.File(filepath, 'r')
@@ -15,13 +16,18 @@ def data_loader(filepath):
     return x_data, y_data
 
 def main():
-    x_test, y_test = data_loader(clean_data_filename)
+    cl_x_test, cl_y_test = data_loader(clean_data_filename)
+    bd_x_test, bd_y_test = data_loader(poisoned_data_filename)
 
     bd_model = keras.models.load_model(model_filename)
 
-    clean_label_p = np.argmax(bd_model.predict(x_test), axis=1)
-    class_accu = np.mean(np.equal(clean_label_p, y_test))*100
-    print('Classification accuracy:', class_accu)
+    cl_label_p = np.argmax(bd_model.predict(cl_x_test), axis=1)
+    clean_accuracy = np.mean(np.equal(cl_label_p, cl_y_test))*100
+    print('Clean Classification accuracy:', clean_accuracy)
+    
+    bd_label_p = np.argmax(bd_model.predict(bd_x_test), axis=1)
+    asr = np.mean(np.equal(bd_label_p, bd_y_test))*100
+    print('Attack Success Rate:', asr)
 
 if __name__ == '__main__':
     main()
